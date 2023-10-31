8752ffbcb719f1267d3b58627baa44347b90cb3e
Паньковский Илья <pankovskiy.im@gmail.com>
2023-10-31 11:51:37 +0300

    complete new task

--- first_and_last_page.py
+++ first_and_last_page.py
@@ -1,1 +1,14 @@
 import requests
+
+url = 'https://parsinger.ru/3.3/4/{number}.html'
+
+with requests.Session() as s:
+    for first_available_page in range(100):
+        if s.get(url=url.format(number=first_available_page)).status_code == 200:
+            break
+    for last_available_page in range(100, 0, -1):        
+        if s.get(url=url.format(number=last_available_page)).status_code == 200:
+            break
+
+print(f"Первая доступная страница: {first_available_page}.html")
+print(f"Последняя доступная страница: {last_available_page}.html")
