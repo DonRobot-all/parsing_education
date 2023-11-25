23e7a0a5dcbd4e553b21362ae44d6c4b5d87e268
Паньковский Илья <pankovskiy.im@gmail.com>
2023-11-25 23:39:13 +0300

    Queue time counter

--- queqe_friend.py
+++ queqe_friend.py
@@ -1,22 +1,30 @@
-# Queue time counter
-def queue(queuers, pos, time=0):
-    for count in range(sum(queuers)):
-        if pos == 0:
-            if queuers[pos] == 1:
-                time += 1
-                return time
-            else:
-                number = queuers.pop(0)
-                queuers.append(number - 1)
-                pos = len(queuers) - 1
-                time += 1
-        else:
-            number = queuers.pop(0)
-            if number != 1:
-                queuers.append(number - 1)
-            pos -= 1
-            time += 1
-        # print(queqe, time, position)
+# # Queue time counter
+# def queue(queuers, pos, time=0):
+#     for count in range(sum(queuers)):
+#         if pos == 0:
+#             if queuers[pos] == 1:
+#                 time += 1
+#                 return time
+#             else:
+#                 number = queuers.pop(0)
+#                 queuers.append(number - 1)
+#                 pos = len(queuers) - 1
+#                 time += 1
+#         else:
+#             number = queuers.pop(0)
+#             if number != 1:
+#                 queuers.append(number - 1)
+#             pos -= 1
+#             time += 1
+#         # print(queqe, time, position)
+
+def queue(queuers,pos):
+    ticket_Need = queuers[pos]
+    frontFriend = queuers[:pos+1]
+    backFriend = queuers[pos+1:]
+    frontFriend = [min(x, ticket_Need) for x in frontFriend]
+    backFriend = [min(x, ticket_Need-1) for x in backFriend]
+    return sum(frontFriend) + sum(backFriend)
 
 
-print(queue([2, 5, 3, 6, 4], 3))
+print(queue([2, 5, 3, 6, 4], 2))
