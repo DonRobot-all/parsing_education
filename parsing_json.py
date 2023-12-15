46be1c2134f1c02b976d54dc7bfd3586d391431d
Паньковский Илья <pankovskiy.im@gmail.com>
2023-12-15 13:42:00 +0300

    parsing json

---
+++ parsing_json.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'https://jsonplaceholder.typicode.com/posts'
+url = 'https://catalog.wb.ru/catalog/electronic22/catalog?TestGroup=no_test&TestID=no_test&appType=1&curr=rub&dest=-2227024&page=3&sort=popular&spp=27&subject=517'
+
+response = requests.get(url=url).json()
+thing = response['data']['products'][0]
+print(thing['name'], thing['priceU'])
